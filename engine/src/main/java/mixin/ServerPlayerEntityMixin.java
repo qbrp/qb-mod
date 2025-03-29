d647f53dd312fa5f2ff1cd17395fbffc9cfe7af4
Трибунал <48561105+lain1wakura@users.noreply.github.com>
2025-03-29 20:17:19 +0300

    Блокировка действий игрока, если тот не авторизован

--- engine/src/main/java/mixin/ServerPlayerEntityMixin.java
+++ engine/src/main/java/mixin/ServerPlayerEntityMixin.java
@@ -2,14 +2,21 @@ package mixin;
 
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import org.qbrp.core.game.player.PlayerManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.annotation.Nullable;
+import java.util.OptionalInt;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
--- engine/src/main/kotlin/org/qbrp/core/game/player/PlayerHandler.kt
+++ engine/src/main/kotlin/org/qbrp/core/game/player/PlayerHandler.kt
@@ -1,9 +1,37 @@
 package org.qbrp.core.game.player
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 import net.minecraft.entity.attribute.EntityAttributeModifier
 import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.util.ActionResult
+import net.minecraft.world.GameMode
+import org.qbrp.core.game.player.events.PlayerChangeGameModeEvent
+import org.qbrp.engine.chat.core.events.MessageReceivedEvent
 
 class PlayerHandler(private val session: ServerPlayerSession) {
+
+    init {
+        // Блокировка смена режима игры, если игрок не авторизован
+        PlayerChangeGameModeEvent.EVENT.register() { player, gameMode ->
+            if (!session.isAuthorized() && gameMode != GameMode.SPECTATOR) {
+                ActionResult.FAIL
+            } else
+                ActionResult.PASS
+        }
+
+        // Блокировка отправки сообщений
+        MessageReceivedEvent.EVENT.register() { message ->
+            if (message.getAuthorEntity() == session.entity && !session.isAuthorized()) {
+                ActionResult.FAIL
+            } else
+                ActionResult.PASS
+        }
+
+        // Обработка тика
+        ServerTickEvents.END_WORLD_TICK.register { server ->
+            handleTick()
+        }
+    }
 
     private fun createAttributeModifierId(type: String = "modifier") = "${session.entity.name.string}-$type}"
 
