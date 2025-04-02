ef5ea3baf248b1a3c9f9ca3c3a6454c570c3ab4d
Трибунал <48561105+lain1wakura@users.noreply.github.com>
2025-04-03 02:27:32 +0300

    Автоматическое сохранение при ошибке инициализации

--- engine/src/main/kotlin/org/qbrp/core/resources/units/TextUnit.kt
+++ engine/src/main/kotlin/org/qbrp/core/resources/units/TextUnit.kt
@@ -7,15 +7,17 @@ open class TextUnit(
     path: Path,
     name: String,
     extension: String,
-    open val data: Data
+    open val data: Data,
 ) : Unit(path.resolve("$name.$extension") as Path), Savable {
     var autoOverwrite: Boolean = false
+    var autoInitialization: Boolean = false
 
     override fun initFile(): Unit {
         val filePath = this.path.toFile()
         if (!filePath.exists()) {
             try { filePath.createNewFile()
             } catch (e: Exception) {
+                if (autoInitialization) save()
                 throw IllegalStateException("Не удалось создать файл: ${filePath.path}", e)
             }
         }
