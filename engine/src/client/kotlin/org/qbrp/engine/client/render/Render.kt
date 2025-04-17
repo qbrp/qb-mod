c663f3f5264a842f3805c58ffee1537dcbb62413
Трибунал <48561105+lain1wakura@users.noreply.github.com>
2025-04-17 07:14:35 +0300

    Отключена ветка рендера

--- engine/src/client/kotlin/org/qbrp/engine/client/render/Render.kt
+++ engine/src/client/kotlin/org/qbrp/engine/client/render/Render.kt
@@ -1,19 +1,12 @@
 package org.qbrp.engine.client.render
 
-import icyllis.modernui.core.Core
-import icyllis.modernui.mc.fabric.MuiFabricApi
-import org.qbrp.engine.client.render.game.chat.PlayerIconRenderer
-import org.qbrp.engine.client.render.hud.HudManager
-import org.qbrp.engine.client.render.screen.Notification
-class Render {
-    companion object {
-        fun openChatScreen() {
-            //MuiFabricApi.openScreen(ChatScreen())
-        }
-    }
-    //val hudManager = HudManager()
+import org.qbrp.engine.client.render.hud.HudInitializer
+
+object Render {
+    //val hudInitializer = HudInitializer()
 
     fun initialize() {
         //PlayerIconRenderer().initialize()
+        //hudRender.initialize()
     }
 }
