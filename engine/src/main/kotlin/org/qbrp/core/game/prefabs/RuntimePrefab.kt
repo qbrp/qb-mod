cac104f250112ce8ff57b961aa870c3f20f9cb2d
Трибунал <48561105+lain1wakura@users.noreply.github.com>
2025-05-05 08:28:44 +0300

    refactor(game-engine): Prefab переименован в RuntimePrefab

--- engine/src/main/kotlin/org/qbrp/core/game/prefabs/RuntimePrefab.kt
+++ engine/src/main/kotlin/org/qbrp/core/game/prefabs/RuntimePrefab.kt
@@ -1,10 +1,8 @@
 package org.qbrp.core.game.prefabs
 
-import org.qbrp.core.game.model.components.Component
-import org.qbrp.core.game.model.components.json.ComponentJsonField
 import org.qbrp.core.game.model.objects.BaseObject
 
-open class Prefab(val id: String, val category: String, val components: MutableList<PrefabField> = mutableListOf(), val tags: MutableList<Tag> = mutableListOf()) {
+open class RuntimePrefab(val id: String, val category: String, val components: MutableList<PrefabField> = mutableListOf(), val tags: MutableList<Tag> = mutableListOf()) {
 
     fun getDefaultTag(): Tag {
         return Tag("default", components)
--- engine/src/main/kotlin/org/qbrp/core/game/prefabs/RuntimePrefabStorage.kt
+++ engine/src/main/kotlin/org/qbrp/core/game/prefabs/RuntimePrefabStorage.kt
@@ -1,17 +1,13 @@
 package org.qbrp.core.game.prefabs
 
-import net.minecraft.world.World
-import org.qbrp.core.game.model.objects.BaseObject
-import org.qbrp.core.game.model.storage.Storage
-
 class RuntimePrefabStorage {
-    val prefabs = mutableMapOf<String, MutableList<Prefab>>()
+    val prefabs = mutableMapOf<String, MutableList<RuntimePrefab>>()
 
-    fun addPrefab(prefab: Prefab) {
+    fun addPrefab(prefab: RuntimePrefab) {
         prefabs.getOrPut(prefab.category) { mutableListOf() }.add(prefab)
     }
 
-    fun getPrefabTag(category: String, prefab: String, tag: String? = null): Prefab.Tag? {
+    fun getPrefabTag(category: String, prefab: String, tag: String? = null): RuntimePrefab.Tag? {
         val pf = prefabs[category]?.find { it.id == prefab }
         if (tag != null) {
             return pf!!.getTag(tag)
