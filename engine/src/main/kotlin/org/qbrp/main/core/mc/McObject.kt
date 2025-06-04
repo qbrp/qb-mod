819ef1661ac6d5ee54d716bc18036143da91a5f3
Трибунал <48561105+lain1wakura@users.noreply.github.com>
2025-06-04 12:54:39 +0300

    fix(mc): mcObject больше не запрашивает lifeCycle и вскоре будет удален

--- engine/src/main/kotlin/org/qbrp/main/core/mc/McObject.kt
+++ engine/src/main/kotlin/org/qbrp/main/core/mc/McObject.kt
@@ -4,9 +4,7 @@ import net.minecraft.util.math.Vec3d
 import org.qbrp.main.core.game.lifecycle.Lifecycle
 import org.qbrp.main.core.game.model.objects.BaseObject
 
-abstract class McObject(
-    lifecycle: Lifecycle<McObject>
-) : BaseObject(lifecycle = lifecycle as Lifecycle<BaseObject>) {
+abstract class McObject() : BaseObject() {
     abstract val pos: Vec3d
     abstract fun getTooltip(): String
 }
